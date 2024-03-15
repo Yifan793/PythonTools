c9a227f887f685529a2ca41e458f68ea7f88e6e1
GRAPECITY\ShaySong <Shay.Song@grapecity.com>
2024-03-15 15:28:50 +0800

    add generate script for 潜客 and 联系人

---
+++ generate_data/generate_0311_2.py
@@ -0,0 +1,90 @@
+from datetime import timedelta
+import mysql.connector
+from faker import Faker
+import requests
+
+fake = Faker("zh_CN")
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="test_0311_2"
+)
+cursor = db.cursor()
+
+
+def generate_qianke_table():
+    insert_query_qianke = "INSERT INTO 潜客表 (ID, 姓名, 公司, 职位, 手机, 电子邮箱, 潜客来源ID, 行业ID, 客户类型ID," \
+                          "地址编号, 详细地址, 负责人, 跟进阶段, 感兴趣产品, 状态ID, FGC_Creator, FGC_CreateDate) " \
+                          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    insert_query_qiankejilucaozuo = "INSERT INTO 潜客记录操作表 (ID, 潜客ID, 操作类型, FGC_Creator, " \
+                                    "FGC_CreateDate) VALUES (%s, %s, %s, %s, %s)"
+
+    cursor.execute("SELECT 编号, 全名 FROM test_0311_2.省市区表")
+    results = cursor.fetchall()
+    address_numbers = [result[0] for result in results]
+    address_full_paths = [result[1] for result in results]
+
+    for i in range(28, 1000001):
+        random_number = fake.random_int(0, 4289)
+        address_number = address_numbers[random_number]
+        address_full_path = address_full_paths[random_number]
+        people = fake.random_elements(elements=('市场人员1', '市场人员2', '市场人员3'), length=1)[0]
+        principal = fake.random_elements(elements=(None, '销售人员1', '销售人员2', '销售人员3'), length=1)[0]
+        products = fake.random_elements(elements=('1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                                                  '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+                                                  '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+                                                  '31', '32', '33', '34', '35', '36', '37', '38'), unique=True)
+
+        time = fake.date_time()
+        cursor.execute(insert_query_qianke,
+                       (i, fake.name(), fake.company(), fake.job(), fake.phone_number(), fake.company_email(),
+                        fake.random_int(1, 8), fake.random_int(1, 24), fake.random_int(1, 5), address_number,
+                        address_full_path + fake.street_address(), principal, fake.random_int(1, 5),
+                        ','.join(products), fake.random_int(1, 4), people, time))
+        cursor.execute(insert_query_qiankejilucaozuo,
+                       (18+i, i, "创建", people, time))
+
+    # 提交更改
+    db.commit()
+
+    # 关闭连接
+    cursor.close()
+    db.close()
+
+
+def generate_lianxiren_table():
+    insert_query_kehu = "INSERT INTO 客户表 (ID, 名称, 电话, 行业ID, 员工数, 地址编号, 详细地址, 客户类型ID, FGC_Creator, " \
+                        "FGC_CreateDate) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    insert_query_lianxiren = "INSERT INTO 联系人表 (ID, 姓名, 职位, 手机, 电子邮箱, 客户ID, FGC_Creator, FGC_CreateDate) " \
+                             "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+
+    cursor.execute("SELECT 编号, 全名 FROM test_0311_2.省市区表")
+    results = cursor.fetchall()
+    address_numbers = [result[0] for result in results]
+    address_full_paths = [result[1] for result in results]
+
+    for i in range(17, 1000001):
+        random_number = fake.random_int(0, 4289)
+        address_number = address_numbers[random_number]
+        address_full_path = address_full_paths[random_number]
+        people = fake.random_elements(elements=('销售人员1', '销售人员2', '销售人员3'), length=1)[0]
+
+        time = fake.date_time()
+        cursor.execute(insert_query_kehu,
+                       (i, fake.company(), fake.phone_number(), fake.random_int(1, 24), fake.random_int(50, 20000),
+                        address_number, address_full_path + fake.street_address(), fake.random_int(1, 5), people, time))
+        cursor.execute(insert_query_lianxiren,
+                       (i-3, fake.name(), fake.job(), fake.phone_number(), fake.company_email(), i, people, time))
+
+    # 提交更改
+    db.commit()
+
+    # 关闭连接
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    generate_lianxiren_table()
